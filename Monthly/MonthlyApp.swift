a4f49fc7c9e9a6b1c597ad180bd92dc06ae1316b
LinhNH2096 <linh.nh@neo-lab.vn>
2024-02-16 15:17:19 +0700

    Initial Commit

---
+++ Monthly/MonthlyApp.swift
@@ -0,0 +1,17 @@
+//
+//  MonthlyApp.swift
+//  Monthly
+//
+//  Created by LinhNguyen DGWA on 16/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct MonthlyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
